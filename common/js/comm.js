29f2608620db5dfe60567e6aebd8c985f8b98b60
parklry <73975006+parklry@users.noreply.github.com>
2023-03-09 13:34:00 +0900

    Update comm.js

--- common/js/comm.js
+++ common/js/comm.js
@@ -195,3 +195,5 @@ Date.prototype.format = function (f) {
 String.prototype.string = function (len) { var s = '', i = 0; while (i++ < len) { s += this; } return s; };
 String.prototype.zf = function (len) { return "0".string(len - this.length) + this; };
 Number.prototype.zf = function (len) { return this.toString().zf(len); };
+//let today = new Date();
+//console.log(today.format("yyyy.MM.dd a/p hh:mm:ss"));
